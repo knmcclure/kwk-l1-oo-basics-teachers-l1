a7fc03d99d907b2f9b4ee3c6c1537f42a9c00c9f
Kyla McClure <kyla.mcclure.16@tfacorps.org>
2018-06-21 03:10:13 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,49 @@
 # book.rb
 # Add you Book class here
+class Book 
+  def initialize(title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author 
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
+
+book_1 = Book.new("Harry Potter")
+
+book_1.genre = "fantasy"
+book_genre = book_1.genre 
+puts book_genre
+
+book_1.page_count = 320
+puts book_1.page_count
